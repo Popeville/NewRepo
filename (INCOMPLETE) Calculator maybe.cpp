acb2297e310741e477bf1470b6cf19d5f232abf4
Popeville <104520208+Popeville@users.noreply.github.com>
2022-04-27 13:05:29 -0400

    Create (INCOMPLETE) Calculator maybe.cpp

---
+++ (INCOMPLETE) Calculator maybe.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int op;
+	float num1, num2;
+	cout << "Please enter an operand: " << endl;
+	cin >> op;
+  
+	cout << "Please enter two operators: " << endl;
+	cin >> num1, num2;
+  
+	switch (op)
+		case ('+')
